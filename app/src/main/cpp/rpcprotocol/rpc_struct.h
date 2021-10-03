a8320a7f11b8df08a12150c4327b5afc308898cb
Null Idle <xenonhydride@gmail.com>
2021-10-03 16:01:16 +0800

    Align rpc_struct to 8 bytes

--- app/src/main/cpp/rpcprotocol/rpc_struct.h
+++ app/src/main/cpp/rpcprotocol/rpc_struct.h
@@ -14,6 +14,7 @@ typedef struct RpcTransactionHeader {
     uint32_t sequence;
     uint32_t funcId;
     uint32_t rpcFlags;
+    uint32_t reserved;
 } RpcTransactionHeader;
 
 constexpr uint32_t RPC_FLAG_EXCEPTION = 0x1;
@@ -21,9 +22,10 @@ constexpr uint32_t RPC_FLAG_EXCEPTION = 0x1;
 typedef struct EventTransactionHeader {
     uint32_t sequence;
     uint32_t eventId;
+    uint32_t eventFlags;
     uint32_t reserved;
 } EventTransactionHeader;
 
 static_assert(sizeof(TrxnPacketHeader) == 8, "TrxnPacketHeader size error");
-static_assert(sizeof(RpcTransactionHeader) == 12, "RpcTransactionHeader size error");
-static_assert(sizeof(EventTransactionHeader) == 12, "EventTransactionHeader size error");
+static_assert(sizeof(RpcTransactionHeader) == 16, "RpcTransactionHeader size error");
+static_assert(sizeof(EventTransactionHeader) == 16, "EventTransactionHeader size error");
