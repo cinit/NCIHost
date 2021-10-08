2931f4873723d808e0d20140279603c6673b75f5
Null Idle <xenonhydride@gmail.com>
2021-10-08 11:58:57 +0800

    Rename ambitious member field name

--- app/src/main/cpp/rpcprotocol/LpcResult.cpp
+++ app/src/main/cpp/rpcprotocol/LpcResult.cpp
@@ -7,26 +7,27 @@
 
 LpcResult LpcResult::fromLpcRespPacketBuffer(const SharedBuffer &buffer) {
     LpcResult result;
-    if (buffer.get() == nullptr || buffer.size() < 24) {
+    constexpr size_t headerSize = sizeof(LpcTransactionHeader);
+    if (buffer.get() == nullptr || buffer.size() < headerSize) {
         return result;
     }
-    if (buffer.size() > 24) {
-        result.mBuffer.ensureCapacity(buffer.size() - 24);
-        memcpy(result.mBuffer.get(), buffer.get(), buffer.size() - 24);
+    if (buffer.size() > headerSize) {
+        result.mArgsBuffer.ensureCapacity(buffer.size() - headerSize);
+        memcpy(result.mArgsBuffer.get(), buffer.at<char>(headerSize), buffer.size() - headerSize);
     }
     result.mErrorCode = buffer.at<LpcTransactionHeader>(0)->errorCode;
     result.mHasException = (buffer.at<LpcTransactionHeader>(0)->rpcFlags
                             & LPC_FLAG_RESULT_EXCEPTION) != 0;
     result.mIsValid = result.mErrorCode != 0
-                      || ArgList(buffer.at<char>(sizeof(LpcTransactionHeader)), buffer.size()).isValid();
+                      || ArgList(buffer.at<char>(headerSize), buffer.size()).isValid();
     return result;
 }
 
 SharedBuffer LpcResult::buildLpcResponsePacket(uint32_t sequence) const {
     SharedBuffer result;
     if (mIsValid) {
-        result.ensureCapacity(sizeof(LpcTransactionHeader) + mBuffer.size());
-        memcpy(result.at<char>(sizeof(LpcTransactionHeader)), mBuffer.get(), mBuffer.size());
+        result.ensureCapacity(sizeof(LpcTransactionHeader) + mArgsBuffer.size());
+        memcpy(result.at<char>(sizeof(LpcTransactionHeader)), mArgsBuffer.get(), mArgsBuffer.size());
         auto *h = result.at<LpcTransactionHeader>(0);
         h->header.length = (uint32_t) result.size();
         h->header.type = TrxnType::TRXN_TYPE_RPC;
--- app/src/main/cpp/rpcprotocol/LpcResult.h
+++ app/src/main/cpp/rpcprotocol/LpcResult.h
@@ -17,7 +17,7 @@ private:
     bool mIsValid = false;
     bool mHasException = false;
     uint32_t mErrorCode = 0;
-    SharedBuffer mBuffer;
+    SharedBuffer mArgsBuffer;
 public:
     [[nodiscard]] inline bool isValid() const noexcept {
         return mIsValid;
@@ -42,7 +42,7 @@ public:
     }
 
     inline void throwException(const RemoteException &exception) {
-        mBuffer = ArgList::Builder().pushArgs(exception.typeId, exception.statusCode, exception.message).build();
+        mArgsBuffer = ArgList::Builder().pushArgs(exception.typeId, exception.statusCode, exception.message).build();
         mHasException = false;
         mErrorCode = 0;
         mIsValid = true;
@@ -54,7 +54,7 @@ public:
     }
 
     inline void returnVoid() {
-        mBuffer = ArgList::Builder().build();
+        mArgsBuffer = ArgList::Builder().build();
         mHasException = false;
         mErrorCode = 0;
         mIsValid = true;
@@ -67,7 +67,7 @@ public:
 
     template<class T>
     void returnResult(const T &result) {
-        mBuffer = ArgList::Builder().push(result).build();
+        mArgsBuffer = ArgList::Builder().push(result).build();
         mHasException = false;
         mErrorCode = 0;
         mIsValid = true;
@@ -75,20 +75,20 @@ public:
 
     template<class T>
     [[nodiscard]] bool getResult(T *result) const {
-        if (!mIsValid || mHasException || mBuffer.get() == nullptr) {
+        if (!mIsValid || mHasException || mArgsBuffer.get() == nullptr) {
             return false;
         }
-        return ArgList(mBuffer.get(), mBuffer.size()).get<T>(result, 0);
+        return ArgList(mArgsBuffer.get(), mArgsBuffer.size()).get<T>(result, 0);
     }
 
     [[nodiscard]] bool getException(RemoteException *exception) const {
-        if (!mIsValid || !mHasException || mBuffer.get() == nullptr) {
+        if (!mIsValid || !mHasException || mArgsBuffer.get() == nullptr) {
             return false;
         }
         uint32_t type = 0;
         uint32_t status = 0;
         std::string msg;
-        if (ArgList args(mBuffer.get(), mBuffer.size());
+        if (ArgList args(mArgsBuffer.get(), mArgsBuffer.size());
                 args.get(&type, 0) || args.get(&status, 1) || args.get(&msg, 2)) {
             *exception = {type, status, msg};
             return true;
@@ -100,7 +100,7 @@ public:
         mIsValid = false;
         mHasException = false;
         mErrorCode = 0;
-        mBuffer.reset();
+        mArgsBuffer.reset();
     }
 
     [[nodiscard]] SharedBuffer buildLpcResponsePacket(uint32_t sequence) const;
